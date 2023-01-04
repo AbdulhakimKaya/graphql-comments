1aebdc019fecd3323aa57158b88ce8b4139e1d8a
KAYA-99 <abdulhakimkaya2199@gmail.com>
2023-01-04 16:46:00 +0300

    added mutation delete and delete all

--- index.js
+++ index.js
@@ -65,6 +65,11 @@ const typeDefs = gql`
         user_id: ID
     }
     
+    
+    type DeleteAllOutput {
+        count: Int!
+    }
+    
     type Query {
         # user
         users: [User!]!
@@ -83,14 +88,20 @@ const typeDefs = gql`
         # User
         createUser(data: CreateUserInput!): User!
         updateUser(id: ID!, data: UpdateUserInput!): User!
+        deleteUser(id: ID!): User!
+        deleteAllUsers: DeleteAllOutput!
         
         # Post
         createPost(data: CreatePostInput!): Post!
         updatePost(id: ID!, data: UpdatePostInput!): Post!
+        deletePost(id: ID!): Post!
+        deleteAllPosts: DeleteAllOutput!
         
         # Comment
         createComment(data: CreateCommentInput!): Comment!
         updateComment(id: ID!, data: UpdateCommentInput!): Comment!
+        deleteComment(id: ID!): Comment!
+        deleteAllComments: DeleteAllOutput!
     }
 `;
 
@@ -119,6 +130,24 @@ const resolvers = {
             })
             return updated_user
         },
+        deleteUser: (parent, {id}) => {
+            const user_index = users.findIndex((user) => user.id === id)
+            if (user_index === -1){
+                throw new Error("User not found")
+            }
+            const deleted_user = users[user_index]
+
+            // ilk parametre hangi index değerinden sonra silme işlemi yapılacağını belirtir
+            // ikinci parametre kaç index silineceğini belirtir
+            users.splice(user_index, 1)
+
+            return deleted_user
+        },
+        deleteAllUsers: () => {
+            const length = users.length
+            users.splice(0, length)
+            return {count: length}
+        },
 
         // Post
         createPost: (parent, {data: {title, user_id}}) => {
@@ -142,6 +171,20 @@ const resolvers = {
             })
             return updated_post
         },
+        deletePost: (parent, {id}) => {
+            const post_index = posts.findIndex((post) => post.id === id)
+            if (post_index === -1){
+                throw new Error("Post not found")
+            }
+            const deleted_post = posts[post_index]
+            posts.splice(post_index,1)
+            return deleted_post
+        },
+        deleteAllPosts: () => {
+            const length = posts.length
+            posts.splice(0, length)
+            return {count:length}
+        },
 
         // Comment
         createComment: (parent, {data}) => {
@@ -163,7 +206,21 @@ const resolvers = {
                 ...data,
             })
             return updated_comment
-        }
+        },
+        deleteComment: (parent, {id}) => {
+            const comment_index = comments.findIndex((comment) => comment.id === id)
+            if (comment_index === -1){
+                throw new Error("Comment not found")
+            }
+            const deleted_comment = comments[comment_index]
+            comments.splice(comment_index, 1)
+            return deleted_comment
+        },
+        deleteAllComments: () => {
+            const length = comments.length
+            comments.splice(0, length)
+            return {count: length}
+        },
     },
 
     Query: {
